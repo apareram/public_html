57108bd25726fa45297dc096753e2b02db5e1797
Alberto Parera <a2091860@ibero.uia.mx>
2024-04-25 09:21:11 -0600

    commit clase pasada

--- proyecto_final/proyectoFinal.php
+++ proyecto_final/proyectoFinal.php
@@ -1,30 +1,22 @@
 <?php
-include 'configs.php';
-require_once "HTML/Template/ITX.php";
+    include 'configs.php';
+    require_once "HTML/Template/ITX.php";
 
-$template = new HTML_Template_ITX('./templates');
-$template->loadTemplatefile("principal.html", true, true);
+    $template = new HTML_Template_ITX('./templates');
 
-$template->setVariable("TITULO", "Sistema de Transporte Universidad Iberoamericana");
-$template->setVariable("CABECERA", "Bienvenido al Sistema de Transporte de la Universidad Iberoamericana");
-$template->setVariable("MENSAJE_BIENVENIDA", "Para acceder a todas las funcionalidades, por favor inicia sesión o crea una nueva cuenta.");
-$template->setVariable("PIE_DE_PAGINA", "&copy; Universidad Iberoamericana - Sistema de Transporte");
-
-// Mostrar el contenido procesado de la plantilla principal
-$template->show();
-
-// Lógica para manejar los botones de acción si se hace clic en ellos
-if (isset($_GET['action'])) {
-    if ($_GET['action'] == 'login') {
-        // Cargar la plantilla de inicio de sesión
-        $template->loadTemplatefile("login.html", true, true);
-        // Configurar las variables necesarias para la plantilla de inicio de sesión y mostrarla
-    } elseif ($_GET['action'] == 'register') {
-        // Cargar la plantilla de registro
-        $template->loadTemplatefile("registro.html", true, true);
-        // Configurar las variables necesarias para la plantilla de registro y mostrarla
+    // Determinar qué plantilla cargar basado en la acción de la URL
+    $action = isset($_GET['action']) ? $_GET['action'] : null;
+    switch ($action) {
+        case 'login':
+            $template->loadTemplatefile("login.html", true, true);
+            break;
+        case 'register':
+            $template->loadTemplatefile("registro.html", true, true);
+            break;
+        default:
+            $template->loadTemplatefile("principal.html", true, true);
+            break;
     }
-    // No olvides mostrar el template actualizado después de cargar la nueva plantilla
+
     $template->show();
-}
 ?>
