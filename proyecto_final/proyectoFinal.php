6628a95144c4ebc3f3c3f40c861b6d5dee15f997
Alberto Parera <a2091860@ibero.uia.mx>
2024-04-25 13:42:43 -0600

    cambios que se hicieron en clase(se arreglo el php y los templates)

--- proyecto_final/proyectoFinal.php
+++ proyecto_final/proyectoFinal.php
@@ -3,19 +3,35 @@
     require_once "HTML/Template/ITX.php";
 
     $template = new HTML_Template_ITX('./templates');
+    $template->loadTemplatefile("principal.html", true, true);
 
-    // Determinar qué plantilla cargar basado en la acción de la URL
-    $action = isset($_GET['action']) ? $_GET['action'] : null;
-    switch ($action) {
-        case 'login':
-            $template->loadTemplatefile("login.html", true, true);
-            break;
-        case 'register':
-            $template->loadTemplatefile("registro.html", true, true);
-            break;
-        default:
-            $template->loadTemplatefile("principal.html", true, true);
-            break;
+    $template->setVariable("TITULO", "Sistema de Transporte Universidad Iberoamericana");
+
+    if (isset($_GET['action'])) {
+
+        $link = mysqli_connect($cfgServer['host'], $cfgServer['user'], $cfgServer['password']) or die('Could not connect: ' . mysqli_error($link));
+	    mysqli_select_db($link, $cfgServer['dbname']) or die("Could not select database");
+
+        if ($_GET['action'] == 'login') {
+            $template->addBlockfile("CONTENIDO", "LOGIN", "login.html");
+            $template->setCurrentBlock("LOGIN");
+            $template->touchBlock("LOGIN");
+            if(isset($_GET['login'])){
+                $username = $_GET['username'];
+                $password = $_GET['password'];
+                $query = "SELECT username FROM Usuarios WHERE username = '$username' AND contraseña = '$password'";
+            }
+        } 
+        elseif ($_GET['action'] == 'register') {
+            $template->addBlockfile("CONTENIDO", "REGISTER", "registro.html");
+            $template->setCurrentBlock("REGISTER");
+            $template->touchBlock("REGISTER");
+        }
+    } 
+    else {
+        $template->addBlockfile("CONTENIDO", "WELCOME", "mensajeBienvenida.html");
+        $template->setCurrentBlock("WELCOME");
+        $template->touchBlock("WELCOME");
     }
 
     $template->show();
