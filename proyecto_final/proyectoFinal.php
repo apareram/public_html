38c6340801b3d42c4e0ce388c74570389e40d3aa
Alberto Parera <a2091860@ibero.uia.mx>
2024-04-30 10:55:20 -0600

    se soluciono el login, ahora el problema es el registrar un nuevo usuario

--- proyecto_final/proyectoFinal.php
+++ proyecto_final/proyectoFinal.php
@@ -8,14 +8,13 @@
 
     $template->setVariable("TITULO", "Sistema de Transporte Universidad Iberoamericana");
 
-    if (isset($_GET['action'])) {
-
+    if ((isset($_GET['action'])) || (isset($_POST['action']))){
         // se hace la conexión a la base de datos
         $link = mysqli_connect($cfgServer['host'], $cfgServer['user'], $cfgServer['password']) or die('Could not connect: ' . mysqli_error($link));
 		mysqli_select_db($link, $cfgServer['dbname']) or die("Could not select database");
 
         // si se pica el boton de login en mensajeBienvenida.html se abrira login.html
-        if ($_GET['action'] == 'login') {
+        if ($_GET['action'] == 'entrar') {
             $template->addBlockfile("CONTENIDO", "LOGIN", "login.html");
             $template->setCurrentBlock("LOGIN");
             $template->touchBlock("LOGIN");
@@ -27,6 +26,7 @@
             $password = mysqli_real_escape_string($link, $_GET['password']);
 
             $query = "SELECT username FROM Usuarios WHERE username = '$username' AND contraseña = '$password'";
+
             $result = mysqli_query($link, $query);
 
             if (mysqli_num_rows($result) > 0) {
@@ -38,45 +38,59 @@
                 $template->parseCurrentBlock("DASHBOARD");
             } else {
                 // Usuario no encontrado, mostrar error y cargar el formulario de inicio de sesión nuevamente.
-                $template->addBlockfile("CONTENIDO", "LOGIN", "login.html");
-                $template->setCurrentBlock("LOGIN");
-                $template->setVariable("MENSAJE_ERROR", "No existe el usuario: '$username'");
-                $template->parseCurrentBlock("LOGIN");
+                $template->addBlockfile("CONTENIDO", "MENSAJE_ERROR", "error.html");
+                $template->setVariable("MENSAJE_ERROR", "Contraseña incorrecta, intenta de nuevo.");
+                $template->setCurrentBlock("MENSAJE_ERROR");
+                $template->parseCurrentBlock("MENSAJE_ERROR");
             }
         }
         // si se pica el boton de registrase en mensajeBienvenida.html se abrira registro.html
-        if ($_GET['action'] == 'register') {
+        if ($_GET['action'] == 'registrar') {
             $template->addBlockfile("CONTENIDO", "REGISTER", "registro.html");
             $template->setCurrentBlock("REGISTER");
             $template->touchBlock("REGISTER");
+        }
 
+        // Registrar nuevo usuario
+        if (isset($_GET['regBot'])) {
             // Validar si el usuario ya existe
-            $username = mysqli_real_escape_string($link, $_POST['username']);
+            $username = mysqli_real_escape_string($link, $_GET['username']);
             $query_check_user = "SELECT username FROM Usuarios WHERE username = '$username'";
             $result_check_user = mysqli_query($link, $query_check_user);
+
             if (mysqli_num_rows($result_check_user) > 0) {
-                $template->setVariable("ERROR", "El nombre de usuario ya está en uso. Por favor, intenta otro.");
+                $template->addBlockfile("CONTENIDO", "MENSAJE_ERROR", "error.html");
+                $template->setVariable("MENSAJE_ERROR", "El nombre de usuario ya está en uso. Por favor, ingresa otro.");
+                $template->setCurrentBlock("MENSAJE_ERROR");
+                $template->parseCurrentBlock("MENSAJE_ERROR");
             } else {
                 // Insertar nuevo usuario en la base de datos
-                $nombre = mysqli_real_escape_string($link, $_POST['nombre']);
-                $aPaterno = mysqli_real_escape_string($link, $_POST['ap_paterno']);
-                $aMaterno = mysqli_real_escape_string($link, $_POST['ap_materno']);
-                $email = mysqli_real_escape_string($link, $_POST['email']);
-                $password = mysqli_real_escape_string($link, $_POST['contraseña']);
-                $calle = mysqli_real_escape_string($link, $_POST['calle']);
-                $numero = mysqli_real_escape_string($link, $_POST['número']);
-                $colonia = mysqli_real_escape_string($link, $_POST['colonia']);
-                $cp = mysqli_real_escape_string($link, $_POST['zip_code']);
+                $nombre = mysqli_real_escape_string($link, $_GET['nombre']);
+                $aPaterno = mysqli_real_escape_string($link, $_GET['ap_paterno']);
+                $aMaterno = mysqli_real_escape_string($link, $_GET['ap_materno']);
+                $email = mysqli_real_escape_string($link, $_GET['email']);
+                $password = mysqli_real_escape_string($link, $_GET['contraseña']);
+                $calle = mysqli_real_escape_string($link, $_GET['calle']);
+                $numero = mysqli_real_escape_string($link, $_GET['número']);
+                $colonia = mysqli_real_escape_string($link, $_GET['colonia']);
+                $cp = mysqli_real_escape_string($link, $_GET['zip_code']);
                 
-                $query_insert_user = "INSERT INTO Usuarios (nombre, ap_paterno, ap_materno, email, username, contraseña, calle, número, colonia, zip_code) VALUES ('$nombre', '$ap_paterno', '$ap_materno', '$email', '$username', '$contraseña', '$calle', '$número', '$colonia', '$zip_code')";
+                $query_insert_user = "INSERT INTO Usuarios (nombre, ap_paterno, ap_materno, username, email, contraseña, calle, número, colonia, zip_code) VALUES ('$nombre', '$aPaterno', '$aMaterno', '$username', '$email', '$password', '$calle', '$numero', '$colonia', '$cp')";
+
                 if (mysqli_query($link, $query_insert_user)) {
-                    $template->setVariable("SUCCESS", "Usuario registrado exitosamente.");
+                    $template->addBlockfile("CONTENIDO", "DASHBOARD", "dashboard.html");
+                    $template->setCurrentBlock("DASHBOARD");
+                    $template->setVariable("USERNAME", $username);
+                    //$template->setVariable("NOTIFICACIONES", obtenerNumeroNotificaciones($username));
+                    $template->parseCurrentBlock("DASHBOARD");
                 } else {
                     $template->setVariable("ERROR", "Error al registrar el usuario. Por favor, inténtalo de nuevo.");
+                    $template->setCurrentBlock("ERROR");
+                    $template->parseCurrentBlock("ERROR");
                 }
             }
-
         }
+
     }else {
         // Cargar la página principal si no se está intentando iniciar sesión o registrar
         $template->addBlockfile("CONTENIDO", "WELCOME", "mensajeBienvenida.html");
