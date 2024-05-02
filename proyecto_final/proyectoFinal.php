2f3f04e481727a843ce74f2ba707c944361f0c9a
Alberto Parera <a2091860@ibero.uia.mx>
2024-05-02 10:33:35 -0600

    commit

--- proyecto_final/proyectoFinal.php
+++ proyecto_final/proyectoFinal.php
@@ -52,9 +52,9 @@
         }
 
         // Registrar nuevo usuario
-        if (isset($_GET['regBot'])) {
+        if (isset($_POST['regBot'])) {
             // Validar si el usuario ya existe
-            $username = mysqli_real_escape_string($link, $_GET['username']);
+            $username = mysqli_real_escape_string($link, $_POST['username']);
             $query_check_user = "SELECT username FROM Usuarios WHERE username = '$username'";
             $result_check_user = mysqli_query($link, $query_check_user);
 
@@ -65,29 +65,40 @@
                 $template->parseCurrentBlock("MENSAJE_ERROR");
             } else {
                 // Insertar nuevo usuario en la base de datos
-                $nombre = mysqli_real_escape_string($link, $_GET['nombre']);
-                $aPaterno = mysqli_real_escape_string($link, $_GET['ap_paterno']);
-                $aMaterno = mysqli_real_escape_string($link, $_GET['ap_materno']);
-                $email = mysqli_real_escape_string($link, $_GET['email']);
-                $password = mysqli_real_escape_string($link, $_GET['contraseña']);
-                $calle = mysqli_real_escape_string($link, $_GET['calle']);
-                $numero = mysqli_real_escape_string($link, $_GET['número']);
-                $colonia = mysqli_real_escape_string($link, $_GET['colonia']);
-                $cp = mysqli_real_escape_string($link, $_GET['zip_code']);
-                
-                $query_insert_user = "INSERT INTO Usuarios (nombre, ap_paterno, ap_materno, username, email, contraseña, calle, número, colonia, zip_code) VALUES ('$nombre', '$aPaterno', '$aMaterno', '$username', '$email', '$password', '$calle', '$numero', '$colonia', '$cp')";
-
-                if (mysqli_query($link, $query_insert_user)) {
-                    $template->addBlockfile("CONTENIDO", "DASHBOARD", "dashboard.html");
-                    $template->setCurrentBlock("DASHBOARD");
-                    $template->setVariable("USERNAME", $username);
-                    //$template->setVariable("NOTIFICACIONES", obtenerNumeroNotificaciones($username));
-                    $template->parseCurrentBlock("DASHBOARD");
-                } else {
-                    $template->setVariable("ERROR", "Error al registrar el usuario. Por favor, inténtalo de nuevo.");
-                    $template->setCurrentBlock("ERROR");
-                    $template->parseCurrentBlock("ERROR");
+                $nombre = mysqli_real_escape_string($link, $_POST['nombre']);
+                $ap_paterno = mysqli_real_escape_string($link, $_POST['ap_paterno']);
+                $ap_materno = mysqli_real_escape_string($link, $_POST['ap_materno']);
+                $email = mysqli_real_escape_string($link, $_POST['email']);
+                $contrasena = mysqli_real_escape_string($link, $_POST['contrasena']);
+                $calle = mysqli_real_escape_string($link, $_POST['calle']);
+                $numero = mysqli_real_escape_string($link, $_POST['numero']);
+                $numero = filter_input(INPUT_POST, 'numero', FILTER_VALIDATE_INT);
+                if ($numero === false) {
+                    // Manejar el error, por ejemplo enviando un mensaje al usuario
+                    echo "El número de exterior proporcionado no es válido.";
+                    return; // Salir del script si hay un error
                 }
+                $colonia = mysqli_real_escape_string($link, $_POST['colonia']);
+                $zip_code = mysqli_real_escape_string($link, $_POST['zip_code']);
+                $zip_code = filter_input(INPUT_POST, 'zip_code', FILTER_VALIDATE_INT);
+                if ($zip_code === false) {
+                    // Manejar el error, por ejemplo enviando un mensaje al usuario
+                    echo "El número de exterior proporcionado no es válido.";
+                    return; // Salir del script si hay un error
+                }
+                
+                $query_insert_user = "INSERT INTO Usuarios (nombre, ap_paterno, ap_materno, username, email, contrasena, calle, numero, colonia, zip_code) VALUES ('$nombre', '$ap_paterno', '$ap_materno', '$username', '$email', '$contrasena', '$calle', '$numero', '$colonia', '$zip_code')";
+                if (!mysqli_query($link, $query_insert_user)) {
+                    echo "Error de inserción: " . mysqli_error($link);
+                } else {
+                    echo "Usuario registrado exitosamente.";
+                }
+
+                $template->addBlockfile("CONTENIDO", "DASHBOARD", "dashboard.html");
+                $template->setCurrentBlock("DASHBOARD");
+                $template->setVariable("USERNAME", $username);
+                //$template->setVariable("NOTIFICACIONES", obtenerNumeroNotificaciones($username));
+                $template->parseCurrentBlock("DASHBOARD");
             }
         }
 
